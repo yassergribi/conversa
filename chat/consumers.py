33306b488737910df4ce91893224da898fbc742b
Yasser Gribi <yasser.gribii@gmail.com>
2024-01-11 19:24:26 +0100

    Receive , Send msg to websocket and save to DB

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,6 +2,12 @@ import json
 
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.utils.timesince import timesince
+
+from account.models import User
+
+from .models import Message, Room
+from .templatetags.chatextras import initials
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -9,12 +15,72 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = f'chat_{self.room_name}'
 
         # join room group
+        await self.get_room()
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
         # leave room
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+
+    async def receive(self, text_data=None):
+        # Receive message FROM websocket
+        text_data_json = json.loads(text_data)
+        type = text_data_json['type']
+        message = text_data_json['message']
+        name = text_data_json['name']
+        agent = text_data_json.get('agent', '')
+
+        print('Receive:', type)
+
+        if type == "message":
+            new_message = await self.create_message(name, message, agent)
+
+            # Send message to room
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'chat_message',
+                    'message':message,
+                    'name':name,
+                    'agent': agent,
+                    'initials': initials(name),
+                    'created_at': timesince(new_message.created_at),
+                }
+            )
+
+    async def chat_message(self, event):
+        #  Send msg TO websocket 
+        await self.send(text_data=json.dumps({
+            'type' : event['type'],
+            'message' : event['message'],
+            'name' : event['name'],
+            'agent' : event['agent'],
+            'initials' : event['initials'],
+            'created_at' : event['created_at'],
+        }))
+    @sync_to_async
+    def get_room(self):
+        self.room = Room.objects.get(uuid=self.room_name)
+
+
+    @sync_to_async
+    def create_message(self, sent_by, message, agent):
+        message = Message.objects.create(body=message, sent_by=sent_by)
+
+        if agent:
+            message.created_by = User.objects.get(pk=agent)
+            message.save()
+
+        self.room.messages.add(message)
+
+        return message
+
+
+
+
+
+
         
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -51,6 +51,53 @@ function getCookie(name) {
     return cookieValue
 }
 
+function sendMessage() {
+    chatSocket.send(JSON.stringify({
+        'type': 'message',
+        'message':chatInputElement.value,
+        'name': chatName
+    }))
+
+    chatInputElement.value = ''
+}
+
+function onChatMessage(data) {
+    console.log('onChatMessage', data)
+
+    if ( data.type == 'chat_message'){
+        if (data.agent){
+            chatLogElement.innerHTML += `
+            <div class="flex w-full mt-2 space-x-3 max-w-md ">
+                <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+                    
+                <div> 
+                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">${data.message}</p>
+                        </div>
+
+                        <span class="text-xs text-gray-500 leading-none"> ${data.created_at} ago</span>
+                </div>
+             </div>  
+            `
+
+        } else {
+            chatLogElement.innerHTML += `
+                <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+                    <div> 
+                        <div class="bg-blue-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">${data.message}</p>
+                        </div>
+
+                        <span class="text-xs text-gray-500 leading-none"> ${data.created_at} ago</span>
+                    </div>
+
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+                </div>
+                `
+        }
+
+    }
+}
 
 async function joinChatRoom() {
     console.log('joinChatRoom')
@@ -82,6 +129,9 @@ async function joinChatRoom() {
 
     chatSocket.onmessage = function(e) {
         console.log('onMessage')
+
+        onChatMessage(JSON.parse(e.data))
+
     }
 
     chatSocket.onopen = function(e) {
@@ -120,6 +170,14 @@ chatJoinElement.onclick = function(e) {
 
     // console.log("cookie", console.log(document.cookie))
 
+
+    return false
+}
+
+chatSubmitElement.onclick = function(e) {
+    e.preventDefault()
+
+    sendMessage()
 
     return false
 }
