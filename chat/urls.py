c01c17fdbf2ef687668575a214a0798df106c2f8
Yasser Gribi <yasser.gribii@gmail.com>
2024-01-28 20:16:20 +0100

    Edit users and delete rooms

--- chat/urls.py
+++ chat/urls.py
@@ -9,5 +9,9 @@ urlpatterns  = [
     path('api/create-room/<str:uuid>/', views.create_room, name='create-room'),
     path('chat-admin/', views.admin, name="admin"),
     path('chat-admin/add-user/', views.add_user, name="add_user"),
-    path('chat-admin/<str:uuid>', views.room, name='room'),
+    path('chat-admin/users/<uuid:uuid>/', views.user_detail, name="user_detail"),
+    path('chat-admin/users/<uuid:uuid>/edit/', views.edit_user, name="edit_user"),
+    path('chat-admin/<str:uuid>/', views.room, name='room'),
+    path('chat-admin/<str:uuid>/delete/', views.delete_room, name='delete_room'),
+
 ]
--- chat/views.py
+++ chat/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 
 from account.models import User
-from account.forms import AddUserForm
+from account.forms import AddUserForm, EditUserForm
 
 from .models import Room
 
@@ -33,6 +33,8 @@ def admin(request):
         "users": users,
     })
 
+
+
 @login_required
 def room(request, uuid):
     room = Room.objects.get(uuid=uuid)
@@ -45,6 +47,56 @@ def room(request, uuid):
     return render(request, 'chat/room.html',{
         'room':room,
     })
+
+@login_required
+def delete_room(request, uuid):
+    if request.user.has_perm('room.delete_room'):
+        room = Room.objects.get(uuid=uuid)
+        room.delete()
+        messages.success(request, "The room was deleted successfully!")
+            
+        return redirect('/chat-admin/')
+    else:
+        messages.error(request, "You don't have access to delete rooms!")
+
+        return redirect('/chat-admin/')
+
+@login_required
+def user_detail(request, uuid):
+    user = User.objects.get(pk=uuid)
+    rooms = user.rooms.all()
+
+    return render(request, 'chat/user_detail.html',{
+        'user': user,
+        'rooms': rooms,
+    })
+
+@login_required
+def edit_user(request, uuid):
+    if request.user.has_perm('user.edit_user'):
+        user = User.objects.get(pk=uuid)
+        
+        if request.method == 'POST':
+            form = EditUserForm(request.POST, instance=user)
+            
+            if form.is_valid():
+                form.save()
+                messages.success(request, "The changes were saved!")
+                return redirect('/chat-admin/')
+        else:
+            form = EditUserForm(instance=user)
+
+        return render(request, 'chat/edit_user.html',{
+            'user': user,
+            'form': form,
+        })
+    else:
+        messages.error(request, "You don't have access to edit users!")
+
+        return redirect('/chat-admin/')
+
+
+    
 
 @login_required
 def add_user(request):
